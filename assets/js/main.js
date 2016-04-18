e5af11e6588c608269a80a80333d91853ca01fe3
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2016-04-18 18:59:12 +0300

    cleaning

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,6 @@
 var currentDate = new Date(),
     pastDate  = new Date('2016-04-12T08:02:03.047Z'),
     diff = (currentDate.getTime() - pastDate.getTime()) / 1000;
-console.log(pastDate);
-console.log(currentDate);
 
 clock = $('.js-timer').FlipClock(diff, {
     clockFace: 'DailyCounter',
