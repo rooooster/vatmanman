b615d2dc4e08b0e1e2b95177cd9ae45b01982bdb
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2016-04-18 18:32:43 +0300

    fix timer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,10 @@
 // FlipClock
 
 var currentDate = new Date(),
-    pastDate  = new Date('2016-04-12 11:02:12'),
+    pastDate  = new Date('2016-04-12T08:02:03.047Z'),
     diff = (currentDate.getTime() - pastDate.getTime()) / 1000;
+console.log(pastDate);
+console.log(currentDate);
 
 clock = $('.js-timer').FlipClock(diff, {
     clockFace: 'DailyCounter',
@@ -16,3 +18,4 @@ $(".youtube-link").on("click", function() {
     window.open('https://www.facebook.com/LuckyLabsCompany/videos/1673526512897392/','_blank');
 
 });
+
